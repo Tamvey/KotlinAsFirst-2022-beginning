4769e9a3746bf7e4f2871ef3e06d584d17c0af36
Matvey <any@gmail.com>
2022-09-04 20:46:16 +0300

    Corrected ageDescription

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -70,7 +70,11 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
 fun ageDescription(age: Int): String {
-	return age.toString() + " год"
+    if(age % 100 in 5..20) return age.toString() + " лет"
+    if(age % 10 == 0) return age.toString() + " лет"
+    if(age % 10 == 1) return age.toString() + " год"
+    if(age % 10 in 2..4) return age.toString() + " года"
+    return age.toString() + " лет"
 }
 
 /**
